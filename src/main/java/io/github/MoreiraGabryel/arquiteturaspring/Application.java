dfe1df5ba3ab754352ca6231fb520b8f96911b08
MoreiraGabryel <marcosgabryel25@gmail.com>
2026-03-19 12:27:20 -0300

    "Primeiras modificações"

---
+++ src/main/java/io/github/MoreiraGabryel/arquiteturaspring/Application.java
@@ -0,0 +1,4 @@
+package io.github.MoreiraGabryel.arquiteturaspring;
+
+public class Application {
+}
--- src/main/java/io/github/MoreiraGabryel/arquiteturaspring/ArquiteturaspringApplication.java
+++ src/main/java/io/github/MoreiraGabryel/arquiteturaspring/ArquiteturaspringApplication.java
@@ -2,12 +2,16 @@ package io.github.MoreiraGabryel.arquiteturaspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ArquiteturaspringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArquiteturaspringApplication.class, args);
+		//SpringApplication.run(ArquiteturaspringApplication.class, args);
+		SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+		builder.run(args);
+
 	}
 
 }
